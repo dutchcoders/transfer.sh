7249b90d7c6c9a2225115473e66d054a4b7a71ad
Uvis Grinfelds <uvis.grinfelds@gmail.com>
2014-11-06 17:12:59 +0100

    preview md image, regular

--- transfersh-web/scripts/main.js
+++ transfersh-web/scripts/main.js
@@ -102,3 +102,61 @@ $(document).ready(function() {
         });
     });
 })();
+
+
+(function() {
+  var copylinkbtn = document.getElementById("copy-link-btn"),
+      copylink = document.getElementById("copy-link-wrapper"),
+      overlay = document.getElementById("overlay");
+
+  copylinkbtn.addEventListener("click", function() {
+    
+    var error = document.getElementsByClassName('error');
+      
+    while (error[0]) {
+      error[0].parentNode.removeChild(error[0]);
+    }
+
+    document.body.className += ' active';
+    
+    copylink.children[2].value = window.location.href;
+    copylink.children[2].focus();
+    copylink.children[2].select();
+  }, false);
+
+  overlay.addEventListener("click", function() {
+    document.body.className = '';
+  }, false);
+
+  copylink.children[2].addEventListener("keydown", function(e) {
+
+    var error = document.getElementsByClassName('error');
+
+    while (error[0]) {
+      error[0].parentNode.removeChild(error[0]);
+    }
+
+    setTimeout(function() {
+
+      if((e.metaKey || e.ctrlKey) && e.keyCode === 67 && isTextSelected(copylink.children[2])) {
+        document.body.className = '';
+      } else if((e.metaKey || e.ctrlKey) && e.keyCode === 67 && isTextSelected(copylink.children[2]) === false) {
+        var error = document.createElement('span');
+        error.className = 'error';
+        var errortext = document.createTextNode('The link was not copied, make sure the entire text is selected.');
+        
+        error.appendChild(errortext);
+        copylink.appendChild(error);
+      }
+    }, 100);
+
+    function isTextSelected(input) {
+      if (typeof input.selectionStart == "number") {
+        return input.selectionStart == 0 && input.selectionEnd == input.value.length;
+      } else if (typeof document.selection != "undefined") {
+        input.focus();
+        return document.selection.createRange().text == input.value;
+      }
+    }
+  }, false);
+})();
