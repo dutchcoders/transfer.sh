ad6cf77017fd0d13cd067fb101ef4e93adb2fd4a
Andrea Spacca <andrea.spacca@gmail.com>
2019-04-07 17:06:27 +0200

    ISSUE-207

--- server/server.go
+++ server/server.go
@@ -317,13 +317,13 @@ func (s *Server) Run() {
 
 	staticHandler := http.FileServer(fs)
 
-	r.PathPrefix("/images/").Handler(staticHandler)
-	r.PathPrefix("/styles/").Handler(staticHandler)
-	r.PathPrefix("/scripts/").Handler(staticHandler)
-	r.PathPrefix("/fonts/").Handler(staticHandler)
-	r.PathPrefix("/ico/").Handler(staticHandler)
-	r.PathPrefix("/favicon.ico").Handler(staticHandler)
-	r.PathPrefix("/robots.txt").Handler(staticHandler)
+	r.PathPrefix("/images/").Handler(staticHandler).Methods("GET")
+	r.PathPrefix("/styles/").Handler(staticHandler).Methods("GET")
+	r.PathPrefix("/scripts/").Handler(staticHandler).Methods("GET")
+	r.PathPrefix("/fonts/").Handler(staticHandler).Methods("GET")
+	r.PathPrefix("/ico/").Handler(staticHandler).Methods("GET")
+	r.PathPrefix("/favicon.ico").Handler(staticHandler).Methods("GET")
+	r.PathPrefix("/robots.txt").Handler(staticHandler).Methods("GET")
 
 	r.HandleFunc("/health.html", healthHandler).Methods("GET")
 	r.HandleFunc("/", s.viewHandler).Methods("GET")
