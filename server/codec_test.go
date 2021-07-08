94f94f27ffa0f1c3e3fa864a2d821db885646bdb
Andrea Spacca <andrea.spacca@gmail.com>
2021-07-08 07:54:43 +0200

    rename benchmark

--- server/codec_test.go
+++ server/codec_test.go
@@ -2,13 +2,13 @@ package server
 
 import "testing"
 
-func BenchmarkEncodeConcat(b *testing.B) {
+func BenchmarkTokenConcat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_ = Token(5) + Token(5)
 	}
 }
 
-func BenchmarkEncodeLonger(b *testing.B) {
+func BenchmarkTokenLonger(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_ = Token(10)
 	}
