f9ffc6a911c1164f101f5f8524570296c45eb68f
Farhan Khursheed <41877347+blind-intruder@users.noreply.github.com>
2022-08-10 23:25:49 -0700

    Fixed improper implementation of content type
    Add text/plain content type if metadata.ContentType is unable to determine the content type of the file

--- server/handlers.go
+++ server/handlers.go
@@ -1034,6 +1034,11 @@ func (s *Server) getHandler(w http.ResponseWriter, r *http.Request) {
 
 	if action == "inline" {
 		disposition = "inline"
+		/*
+		metadata.ContentType is unable to determine the type of the content, 
+		So add text/plain in this case to fix XSS related issues/
+		*/
+		contentType := "text/plain"
 	} else {
 		disposition = "attachment"
 	}
